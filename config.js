525edb91a005718d50132298c4e1faa6fc9ee092
= <=>
2026-08-19 16:10:55 +0530

    Initial Commit - weather app

---
+++ config.js
@@ -0,0 +1,4 @@
+const configs = {
+    api_key: "95679c5ad2c94aaeb1122700261808",
+    api_endpt: "http://api.weatherapi.com/v1/current.json?key=${configs.api_key}&q=Pune&aqi=yes"
+}
---
+++ script.js
@@ -0,0 +1,63 @@
+const form = document.getElementById('weather-form');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const in_place = document.getElementById('input_city').value;
+
+    const error = document.getElementById('error-messages');
+
+    if(in_place === "") {
+        alert("Please enter a city name.");
+        return;
+    }
+
+    //Get weather data
+
+        error.innerText = 'Loading...'
+
+        document.getElementById('search_btn').hidden = true;
+
+
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${configs.api_key}&q=${in_place}&aqi=yes`)
+    .then(response => response.json())
+    .then(res => {
+
+        error.style.color = 'green';
+        error.innerText = 'Data fetched successfully!'
+
+        // Display the weather data on the page
+        
+        const place = document.getElementById('out_place');
+        const region = document.getElementById('out_region');
+        const country = document.getElementById('out_country');
+        const local_time = document.getElementById('out_local_time');
+        const temp = document.getElementById('out_temp');
+        const condition = document.getElementById('out_condition');
+        const humidity = document.getElementById('out_humidity');
+        const wind_speed = document.getElementById('out_wind_speed');
+
+        place.innerText = res.location.name;
+        region.innerText = res.location.region;
+        country.innerText = res.location.country;
+        local_time.innerText = res.location.localtime;
+        temp.innerText = `${res.current.temp_c}°C`;
+        condition.innerText = res.current.condition.text;
+        humidity.innerText = `${res.current.humidity}%`;
+        wind_speed.innerText = `${res.current.wind_kph} km/h`;
+
+        document.getElementById('weather-statistics').style.display = 'block';
+        document.getElementById('search_btn').hidden = false;
+
+        console.log(res);
+    })
+    .catch(error => {
+        console.log(error)
+        error.style.color = 'red';
+        error.innerText = 'Error fetching weather data. Please try again later.';
+    })
+
+    
+    
+
+})
